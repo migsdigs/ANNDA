6ee127fa49de3c06ff4ec1f8115985f090285b8c
Miguel Garcie Naude <miguelito2384@gmail.com>
2022-01-21 12:57:32 +0100

    shuffled class

--- single_layer_perceptron/classification.m
+++ single_layer_perceptron/classification.m
@@ -1,8 +1,8 @@
 %% Generate data to be used for binary classification
 
 n = 100; % number of samples per class
-mA = [ 5.0, 5.0]; sigmaA = 0.5; % x,y centers of normal distribution of class A
-mB = [-5.0, -5.0]; sigmaB = 0.5; % x,y centers of normal distribution of class B
+mA = [ 5.0, 5.0]; sigmaA = 1.5; % x,y centers of normal distribution of class A
+mB = [-5.0, -5.0]; sigmaB = 1.5; % x,y centers of normal distribution of class B
 
 classA(1,:) = randn(1,n) .* sigmaA + mA(1); % x values class A
 classA(2,:) = randn(1,n) .* sigmaA + mA(2); % y values class A
@@ -11,4 +11,6 @@ classB(1,:) = randn(1,n) .* sigmaB + mB(1);
 classB(2,:) = randn(1,n) .* sigmaB + mB(2);
 
 % Merge class A and B into single class and shuffle
-class = 0;
+class = [classA, classB];
+class = class(:, randperm(size(class, 2)));
+
